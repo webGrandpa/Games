00aff5199b48dff736767f20ec467ced0dd51348
giorgi khiladze <goga.khilo@gmail.com>
2025-12-27 23:52:59 +0400

    refine old game

--- GuessMyNumber/animation.js
+++ GuessMyNumber/animation.js
@@ -1,31 +1,19 @@
 const cloudImages = document.querySelectorAll('.cloud-set img');
 const roadElements = document.querySelectorAll('.road-element');
-const marioElements = document.querySelectorAll('.mario-elements > div');
+
 
 function setRandomCloudPosition(imageElement) {
-
-    const randomTop = Math.random() * 55;
+    const randomTop = Math.random() * 60;
     imageElement.style.top = randomTop + '%';
-
-    const randomLeft = Math.random() * 100;
+    
+    const randomLeft = Math.random() * 90; 
     imageElement.style.left = randomLeft + '%';
 }
 
 function setRandomRoadElementPosition(imageElement) {
-
-    const randomLeft = Math.random() * 100;
+    const randomLeft = Math.random() * 90; 
     imageElement.style.left = randomLeft + '%';
-
-}
-
-function setRandomMarioElementPosition(element) {
-    const randomLeft = Math.random() * 100;
-    element.style.left = randomLeft + '%';
 }
 
 cloudImages.forEach(setRandomCloudPosition);
-
 roadElements.forEach(setRandomRoadElementPosition);
-
-marioElements.forEach(setRandomMarioElementPosition);
-
